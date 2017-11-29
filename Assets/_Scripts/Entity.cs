2647a84d4f514e0e232682ac597724775bddf36f
Colton <ColtonJamesCampbell@gmail.com>
2017-11-29 19:50:33 -0400

    Setup basic folders and scripts

---
+++ Assets/_Scripts/Entity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Entity : MonoBehaviour, iDamagable
+{
+    public float health;
+
+    public void Damage(float amount)
+    {
+
+    }
+}
