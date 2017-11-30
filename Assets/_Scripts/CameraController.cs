912a60922a0e1cd769db327bd17e368685452db0
Colton <ColtonJamesCampbell@gmail.com>
2017-11-29 21:12:25 -0400

    Created player and camera
    Added a player script with movement controls. Added a camera controller with mouse controls and basic configuration.

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 offset;
+    public float maxPitch;
+    public AnimationCurve pitchHeightCurve;
+
+    Transform cameraTransform;
+    float pitch = 0.0f;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        cameraTransform = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Rotate();
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 rotatedOffset = (this.transform.forward * offset.z) + (this.transform.right * offset.x) + (this.transform.up * offset.y) + (this.transform.up * PitchHeight(pitch));
+
+        cameraTransform.position = this.transform.position + rotatedOffset;
+
+    }
+
+    void Rotate()
+    {
+        //Rotate player
+        this.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+
+        //Pitch camera
+        pitch = Mathf.Clamp(pitch - Input.GetAxis("Mouse Y"), 0f, maxPitch);
+
+        //Set camera rotation
+        cameraTransform.rotation = Quaternion.Euler(pitch, this.transform.rotation.eulerAngles.y, 0f);
+    }
+
+    float PitchHeight(float currentPitch)
+    {
+        return pitchHeightCurve.Evaluate(currentPitch / maxPitch);
+    }
+
+}
