81eac5e69b858527c0458903337a8d5841f4dc59
DrBrandon <brandon_lange637@hotmail.com>
2017-11-30 01:32:40 -0400

    Made an enemy pathfinding routine

---
+++ Assets/_Scripts/EnemyTracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+    public Transform target;
+
+	void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
