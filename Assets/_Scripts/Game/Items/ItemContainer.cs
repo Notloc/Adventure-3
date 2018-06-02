783030b816d9c6ae7a1d23befc5db3b724babc1e
Colton <ColtonJamesCampbell@gmail.com>
2018-06-02 14:10:18 -0300

    Created the skeleton of the inventory system

---
+++ Assets/_Scripts/Game/Items/ItemContainer.cs
@@ -0,0 +1,15 @@
+﻿namespace Adventure.Game.Items
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [System.Serializable]
+    public class ItemContainer
+    {
+        [SerializeField] ItemSlot[] itemSlots;
+
+
+
+    }
+}
---
+++ Assets/_Scripts/Game/Items/ItemSlot.cs
@@ -0,0 +1,22 @@
+﻿namespace Adventure.Game.Items
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [System.Serializable]
+    public class ItemSlot
+    {
+        public string name
+        {
+            get
+            {
+                return item.name;
+            }
+        }
+
+        [SerializeField] Item item;
+        [SerializeField] int count;
+
+    }
+}
